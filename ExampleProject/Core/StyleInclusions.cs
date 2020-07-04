65c5250848566dbeb3d044cba7f3dbc604b40b09
Steve <steve.vernyi@gmail.com>
2020-07-04 02:39:25 -0400

    Removes unused components

--- ExampleProject/Core/StyleInclusions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace ExampleProject.Core
-{
-    public class StyleInclusions : HashSet<string>
-    {
-        
-    }
-}
--- ExampleProject/Startup.cs
+++ ExampleProject/Startup.cs
@@ -25,8 +25,6 @@ namespace ExampleProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ScriptInclusions>();
-            services.AddScoped<StyleInclusions>();
             services.AddScoped<ComponentContext>(); 
             services.AddControllersWithViews();
         }
